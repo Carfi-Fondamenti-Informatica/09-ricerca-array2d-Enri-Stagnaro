9e5fbd3b67fa2a061130339597ff581b3bef0d6d
Enri-Stagnaro <115564859+Enri-Stagnaro@users.noreply.github.com>
2022-12-16 13:44:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
 
+#include "lib.h"
+
+bool funzione(char a[10][20], char b[20], int &pos) {
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            if (a[j] != a[i][j]) {
+                j = 20;
+            } else if(j==19){
+                pos = i;
+                return true;
+            }
+        }
+    }
+    return false;
+}
